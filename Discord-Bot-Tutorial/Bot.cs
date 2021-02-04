d45b029e758a4a33739f4e6cd3e8f00951f3ad6e
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-02-05 01:24:57 +0400

    C# Discord Bot - Setting Up - Part 1

---
+++ Discord-Bot-Tutorial/Bot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Discord_Bot_Tutorial
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+
+            using (var fs = File.OpenRead("config.json")) 
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                    json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect =  true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[]{configJson.Prefix},
+                EnableDms = false,
+                EnableMentionPrefix = true
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient client, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Discord-Bot-Tutorial/ConfigJson.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Discord_Bot_Tutorial
+{
+    struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
