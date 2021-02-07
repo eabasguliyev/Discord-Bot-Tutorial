3fbc2df82ab9cc717763aed4997d9488910d3aab
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-02-08 03:14:02 +0400

    Discord Bot - Creating A Dialogue Handler - Part 7

---
+++ Discord-Bot-Tutorial/Handlers/Dialogue/DialogueHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord_Bot_Tutorial.Handlers.Steps;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace Discord_Bot_Tutorial.Handlers.Dialogue
+{
+    public class DialogueHandler
+    {
+        private readonly DiscordClient _client;
+        private readonly DiscordChannel _channel;
+        private readonly DiscordUser _user;
+        private IDialogueStep _currentStep;
+        public DialogueHandler(DiscordClient client, DiscordChannel channel, DiscordUser user,
+            IDialogueStep startingStep)
+        {
+            _client = client;
+            _channel = channel;
+            _user = user;
+            _currentStep = _currentStep;
+        }
+
+        private readonly List<DiscordMessage> messages = new List<DiscordMessage>();
+
+
+        public async Task<bool> ProcessDialogue()
+        {
+            while(_currentStep != null)
+            {
+                _currentStep.OnMessageAdded += message => messages.Add(message);
+
+                bool cancled = await _currentStep.ProcessStep(_client, _channel, _user).ConfigureAwait(false);
+
+                if (cancled)
+                {
+                    await DeleteMessages().ConfigureAwait(false);
+                    var cancelEmbed = new DiscordEmbedBuilder()
+                    {
+                        Title = "The Dialogue Has Successfully Been Cancelled",
+                        Description = _user.Mention,
+                        Color = DiscordColor.Green
+                    };
+
+                    await _channel.SendMessageAsync(cancelEmbed).ConfigureAwait(false);
+                    return false;
+                }
+
+                _currentStep = _currentStep.NextStep;
+            }
+
+            await DeleteMessages().ConfigureAwait(false);
+            return true;
+        }
+
+        private async Task DeleteMessages()
+        {
+            if (_channel.IsPrivate)
+            {
+                return;
+            }
+
+            foreach (var discordMessage in messages)
+            {
+                await discordMessage.DeleteAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ Discord-Bot-Tutorial/Handlers/Dialogue/Steps/TextStep.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace Discord_Bot_Tutorial.Handlers.Dialogue.Steps
+{
+    public class TextStep:DialogueStepBase
+    {
+        private readonly IDialogueStep _nextStep;
+        private readonly int? _minLength;
+
+        private readonly int? _maxLength;
+
+        public TextStep(string content, IDialogueStep nextStep, int? minLength = null, int? maxLength =null):base(content)
+        {
+            _nextStep = nextStep;
+            _minLength = minLength;
+            _maxLength = maxLength;
+        }
+
+        public Action<string> OnValidResult { get; set; } = delegate { };
+
+        public override IDialogueStep NextStep => _nextStep;
+
+        public override async Task<bool> ProcessStep(DiscordClient client, DiscordChannel channel, DiscordUser user)
+        {
+            var embedBuilder = new DiscordEmbedBuilder()
+            {
+                Title = $"Please Respond Below",
+                Description = $"{user.Mention}, {_content}",
+            };
+
+            embedBuilder.AddField("To Stop The Dialogue",
+                "Use the ?cancel command");
+
+
+            if(_minLength.HasValue)
+            {
+                embedBuilder.AddField("Min Length: ", $"{_minLength.Value} characters.");
+            }
+
+            if (_maxLength.HasValue)
+            {
+                embedBuilder.AddField("Max Length: ", $"{_minLength.Value} characters.");
+            }
+
+            var interactivity = client.GetInteractivity();
+
+            while (true)
+            {
+                var embed = await channel.SendMessageAsync(embedBuilder).ConfigureAwait(false);
+                OnMessageAdded(embed);
+
+                var messageResult = await interactivity
+                    .WaitForMessageAsync(x => x.ChannelId == channel.Id && x.Author.Id == user.Id)
+                    .ConfigureAwait(false);
+
+                OnMessageAdded(messageResult.Result);
+
+                if (messageResult.Result.Content.Equals("?cancel"))
+                {
+                    return true;
+                }
+
+                if (_minLength.HasValue)
+                {
+                    if (messageResult.Result.Content.Length < _minLength.Value)
+                    {
+                        await TryAgain(channel,
+                                $"Your input is {_minLength.Value - messageResult.Result.Content.Length} character too short")
+                            .ConfigureAwait(false);
+                        continue;
+                    }
+                }
+
+                if (_maxLength.HasValue)
+                {
+                    if (messageResult.Result.Content.Length > _maxLength.Value)
+                    {
+                        await TryAgain(channel,
+                                $"Your input is {messageResult.Result.Content.Length - _maxLength.Value} character too long")
+                            .ConfigureAwait(false);
+                        continue;
+                    }
+                }
+
+                OnValidResult(messageResult.Result.Content);
+
+                return false;
+            }
+        }
+    }
+}
